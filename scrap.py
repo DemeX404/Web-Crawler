c38cbfe3ab7262e0926087dc439096f058cf5498
Dany <damexica@gmail.com>
2022-07-29 22:32:42 +0200

    filter by points and 5<= words in the title

--- scrap.py
+++ scrap.py
@@ -17,7 +17,7 @@ list_search = list()
 count = 0
 
 for i in id:
-    if count < 10:
+    if count < 30:
         entries = soup.find_all('tr', id=i['id'])
         comments = soup.find_all('a', href='item?id='+ i['id'])
         points = soup.find_all('span', id='score_'+ i['id'])
@@ -34,11 +34,12 @@ for i in id:
     count += 1
 
 #All searches
-# for search in list_search:
-#     print(search)
+for search in list_search:
+    print(search)
 
 #Filter Comments
 filter_comments = list()
+temp_list = list()
 
 for element_list in list_search:
     if len(element_list.entry.split()) > 6:
@@ -47,7 +48,6 @@ for element_list in list_search:
             num_comments = int(num_comments)
             if len(filter_comments) == 0:
                 filter_comments.append(element_list)
-
             else:
                 count = 0
                 controlInsert = False
@@ -59,9 +59,42 @@ for element_list in list_search:
                         break
                     count += 1
         else:
-            filter_comments.append(element_list)
+            temp_list.append(element_list)
 
+
+
+filter_comments.extend(temp_list)
+print('List 5> and ordered by comments')
 for x in filter_comments:
+    print(x)
+
+
+#Filter Points
+filter_points = list()
+
+for element_list in list_search:
+    if len(element_list.entry.split()) <= 6:
+        num_points = element_list.points.split()[0]
+        if (num_points.isdigit()):
+            num_points = int(num_points)
+            if len(filter_points) == 0:
+                filter_points.append(element_list)
+
+            else:
+                count = 0
+                controlInsert = False
+
+                for comments_list in filter_points:
+                    if(num_points > int(comments_list.comments.split()[0])):
+                        filter_points.insert(count, element_list)
+                        controlInsert = True
+                        break
+                    count += 1
+        else:
+            filter_points.append(element_list)
+
+print('List 5<= and ordered by points')
+for x in filter_points:
     print(x)
 
 
